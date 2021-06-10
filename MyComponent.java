1e655b68c0af65c07529de23fdee9f0fdc2f5907
RITESH MITTAL <riteshmittal.virgo@gmail.com>
2021-06-10 15:51:15 +0530

    Add files via upload

---
+++ MyComponent.java
@@ -0,0 +1,9 @@
+package com.adobe.aem.guides.wknd.spa.react.core.models;
+
+import com.adobe.cq.export.json.ComponentExporter;
+
+public interface MyComponent extends ComponentExporter{
+
+    public String getText();
+
+}
